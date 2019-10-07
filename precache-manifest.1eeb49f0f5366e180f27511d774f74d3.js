fd26b5b35160c62eb3bb0730b089b55db8dd3b9a
MOSES <MOSESLAZARO2019@outlook.com>
2019-10-07 19:14:13 +0300

    Updates

---
+++ precache-manifest.1eeb49f0f5366e180f27511d774f74d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a87edf25abbbe688e07a72337f24da7",
+    "url": "/Animated-form/index.html"
+  },
+  {
+    "revision": "992011de2cb533603f68",
+    "url": "/Animated-form/static/css/main.0df21b3e.chunk.css"
+  },
+  {
+    "revision": "04a4f95349c544b41630",
+    "url": "/Animated-form/static/js/2.0e7f98b2.chunk.js"
+  },
+  {
+    "revision": "992011de2cb533603f68",
+    "url": "/Animated-form/static/js/main.9cc37f3f.chunk.js"
+  },
+  {
+    "revision": "a517f1b40585d0868e7f",
+    "url": "/Animated-form/static/js/runtime-main.9c127e13.js"
+  }
+]);
